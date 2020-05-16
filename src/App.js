1795aed65b9856c2c8e1af81d0640239ddb1099e
Vijaya Mishra <vijayamishra1709@gmail.com>
2020-05-16 19:55:05 +0530

    added face detect model using clarifai api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'ef8b1668d7c14b39914feb2c603a2891'
+});
 
 const particlesOptions = {
   line_linked: {
@@ -19,6 +25,36 @@ const particlesOptions = {
   }
 }
 class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    //console.log(event.target.value);
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    // console.log('click');
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(
+        function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,13 +64,14 @@ class App extends Component{
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition />*/}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
 }
-// particlesJS.load('particles-js', 'assets/particles.json', function() {
-//   console.log('callback - particles.js config loaded');
-// });
+
 export default App;
