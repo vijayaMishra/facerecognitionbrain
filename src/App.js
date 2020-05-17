e73786bc74d16996322255092394209a95aa5a99
Vijaya Mishra <vijayamishra1709@gmail.com>
2020-05-17 16:58:49 +0530

    fixed signout

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component{
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange}/> 
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/> 
         { this.state.route === 'home' 
           ? <div> 
               <Logo />
